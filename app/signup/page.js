c6795ed238f7bb044791a7be299a5ff8b1e16b79
omertopak <omertopak1@gmail.com>
2025-01-10 17:36:36 +0000

    up

---
+++ app/signup/page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import SignUp from '../(components)/SignUp'
+
+const page = () => {
+  return (
+    <div className='w-full h-full'><SignUp/></div>
+  )
+}
+
+export default page
