065a798ad82ae687b1b98cea900c0478aed583e2
omertopak <omertopak1@gmail.com>
2025-01-23 02:46:01 +0000

    up

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,23 @@
 "use client"
-
 import React from 'react'
+import { useState,useEffect } from 'react';
+import useAuthStore from '../lib/stores/authStore';
+
+
+
 
 const page = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const { setToken,login } = useAuthStore(state => state); 
+
+  // const handleLogin = async () => {
+  //   login({"email":"omer@gmail.com", "password":"omeromer"});
+  // };
+  const data = {"email":"omer@gmail.com", "password":"omeromer"}
+    useEffect(() => {
+      login();
+    }, [])
   return (
     <section className="bg-slate-200 dark:bg-gray-900 w-full h-full">
   <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
@@ -31,7 +46,7 @@ const page = () => {
                       </div>
                       <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
                   </div>
-                  <button type="submit" className="w-full text-gray-700 bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+                  <button type="submit" onSubmit={()=>handleLogin()} className="w-full text-gray-700 bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
                   <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                       Don’t have an account yet? <a href="#" className="font-medium text-gray-700 hover:underline dark:text-primary-500">Sign up</a>
                   </p>
--- app/register/page.js
+++ app/register/page.js
@@ -2,8 +2,13 @@
 
 import React from 'react'
 import SignUp from '../(components)/SignUp'
-
+import useAuthStore from '@/lib/stores/authStore'
+import { useEffect } from 'react'
 const page = () => {
+  const{register} = useAuthStore(state => state);
+  useEffect(() => {
+      register();
+    }, [])
   return (
     <div className='w-full h-full'><SignUp/></div>
   )
